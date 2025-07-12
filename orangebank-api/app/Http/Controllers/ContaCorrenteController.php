d7e49a989d9de909b01d9d7134f17080d77c4e42
Cristian <cristian.braz97@gmail.com>
2025-07-12 13:19:54 -0300

    renomeia variavel saldo para saldo atual, cria nova migration de extrato

--- orangebank-api/app/Http/Controllers/ContaCorrenteController.php
+++ orangebank-api/app/Http/Controllers/ContaCorrenteController.php
@@ -18,7 +18,7 @@ class ContaCorrenteController extends Controller
 
     public function depositarSaldo(Request $request){
         $corrente=ContaCorrente::where('user_id',auth()->user()->id)->first();
-        if($corrente == null && $request->valor >0){
+        if($corrente == null && $request->valor > 0){
             $corrente=new ContaCorrente;
             $corrente->saldo=$request->valor;
             $corrente->user_id=auth()->user()->id;
@@ -28,24 +28,10 @@ class ContaCorrenteController extends Controller
             'status'=>'ok'
         ], 200);
         }elseif($corrente != null && $request->valor > 0){
-            $saldo=$corrente->saldo;
-            $corrente->saldo=($request->valor+$saldo);
+            $saldoAtual=$corrente->saldo;
+            $corrente->saldo=($request->valor+$saldoAtual);
             $corrente->save();
         }
-        // if($request->valor > 0){
-        //     $corrente->saldo=$request->$valor;
-        //     $corrente->user_id=auth()->user()->id;
-        //     $corrente->save();
-        //     return response()->json([
-        //     'response'=>'você depositou '.$corrente->valor. ' em sua conta corrente',
-        //     'status'=>'ok'
-        // ], 200);
-        // }else{
-        //     return response()->json([
-        //         'response'=>'operação inválida.',
-        //         'status'=>'error'
-        //     ],500);
-        // } 
 
     }
 }
